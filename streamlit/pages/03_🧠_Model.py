728822bfdac21cf868ef6ed57d71b15d931024c5
shztodka <katarzyna.zbroinska@gmail.com>
2023-04-10 20:08:08 +0200

    models chart

--- streamlit/pages/03_🧠_Model.py
+++ streamlit/pages/03_🧠_Model.py
@@ -69,7 +69,7 @@ tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["Numeric Data", "Features di
 
 with tab1:
    st.header("Numeric Data")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+   st.image('images/models.png')
 
 with tab2:
    st.header("A dog")
